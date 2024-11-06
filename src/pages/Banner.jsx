85fd258dfaa213b0ebcfa0dc809322ac858dd913
asifalam515 <asibulalam515@gmail.com>
2024-11-07 01:43:13 +0600

    banner updated

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -1,13 +1,28 @@
 const Banner = () => {
   return (
-    <div className="carousel w-full h-80 max-w-7xl mx-auto">
-      <div id="slide1" className="carousel-item relative w-full h-full">
+    <div className="carousel w-full h-[600px]">
+      <div id="slide1" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/n1p7QTR/public-health-concept-illustration-114360-8989.jpg"
-          className="w-full h-full object-cover"
+          src="https://i.ibb.co.com/xgcwPzm/medical-care-concept-illustration-114360-4961.jpg"
+          className="w-full rounded-xl"
         />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide4" className="btn btn-circle">
+        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold">Care For EveryOne</h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-outline btn-secondary">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide4" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide2" className="btn btn-circle">
@@ -15,13 +30,28 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide2" className="carousel-item relative w-full h-full">
+      <div id="slide2" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/fqskpdD/hug-concept-illustration-114360-25662.jpg"
-          className="w-full h-full object-cover"
+          src="https://i.ibb.co.com/n1p7QTR/public-health-concept-illustration-114360-8989.jpg"
+          className="w-full rounded-xl"
         />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide1" className="btn btn-circle">
+        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold">Care For EveryOne</h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-outline btn-secondary">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide1" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide3" className="btn btn-circle">
@@ -29,13 +59,28 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide3" className="carousel-item relative w-full h-full">
+      <div id="slide3" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/P6gFR8N/blood-donation-banner-template-23-2149170486.jpg"
-          className="w-full h-full object-cover"
+          src="https://i.ibb.co.com/fqskpdD/hug-concept-illustration-114360-25662.jpg"
+          className="w-full rounded-xl"
         />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide2" className="btn btn-circle">
+        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold">Care For EveryOne</h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-outline btn-secondary">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide2" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide4" className="btn btn-circle">
@@ -43,13 +88,28 @@ const Banner = () => {
           </a>
         </div>
       </div>
-      <div id="slide4" className="carousel-item relative w-full h-full">
+      <div id="slide4" className="carousel-item relative w-full">
         <img
-          src="https://i.ibb.co/xgcwPzm/medical-care-concept-illustration-114360-4961.jpg"
-          className="w-full h-full object-cover"
+          src="https://i.ibb.co.com/P6gFR8N/blood-donation-banner-template-23-2149170486.jpg"
+          className="w-full rounded-xl"
         />
-        <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-          <a href="#slide3" className="btn btn-circle">
+        <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)]">
+          <div className="text-white space-y-7 pl-12 w-1/2">
+            <h2 className="text-6xl font-bold">Care For EveryOne</h2>
+            <p>
+              There are many variations of passages of available, but the
+              majority have suffered alteration in some form
+            </p>
+            <div>
+              <button className="btn btn-primary mr-5">Discover More</button>
+              <button className="btn btn-outline btn-secondary">
+                Latest Project
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+          <a href="#slide3" className="btn btn-circle mr-5">
             ❮
           </a>
           <a href="#slide1" className="btn btn-circle">
